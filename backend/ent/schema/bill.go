dd541ac083eec04a10d1ebe5aa8f4288e9891008
NutchaPK <theuoon9899@gmail.com>
2020-12-21 19:59:49 +0700

    entity ของ bill
    สร้าง Entity Bill,Financailofficer,Paytype

---
+++ backend/ent/schema/bill.go
@@ -0,0 +1,28 @@
+package schema
+
+import (
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
+)
+
+// Bill holds the schema definition for the Bill entity.
+type Bill struct {
+	ent.Schema
+}
+
+// Fields of the Bill.
+func (Bill) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("Amount").NotEmpty(),
+		field.Time("Date"),
+	}
+}
+
+// Edges of the Bill.
+func (Bill) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("paytype", Paytype.Type).Ref("bills").Unique(),
+		edge.From("officer", Financialofficer.Type).Ref("bills").Unique(),
+	}
+}
---
+++ backend/ent/schema/paytype.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
+)
+
+// Paytype holds the schema definition for the Paytype entity.
+type Paytype struct {
+	ent.Schema
+}
+
+// Fields of the Paytype.
+func (Paytype) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("paytype").NotEmpty(),
+	}
+}
+
+// Edges of the Paytype.
+func (Paytype) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("bills", Bill.Type).StorageKey(edge.Column("paytype_id")),
+	}
+}
