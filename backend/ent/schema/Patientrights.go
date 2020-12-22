b539f67ca154cd34dbd2803fdf4d58522c9cf265
khatadet <69413766+khatadet@users.noreply.github.com>
2020-12-22 15:58:11 +0700

    add schema

--- backend/ent/schema/Patientrights.go
+++ backend/ent/schema/Patientrights.go
@@ -1,6 +1,11 @@
 package schema
 
-import "github.com/facebookincubator/ent"
+import (
+    
+    "github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+	"github.com/facebookincubator/ent/schema/edge"
+)
 
 // Patientrights holds the schema definition for the Patientrights entity.
 type Patientrights struct {
@@ -9,10 +14,33 @@ type Patientrights struct {
 
 // Fields of the Patientrights.
 func (Patientrights) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("PermissionDate"),
+		
+			
+    }
 }
-
 // Edges of the Patientrights.
 func (Patientrights) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+
+		
+		edge.From("PatientrightsPatientrightstype", Patientrightstype.Type).
+			Ref("PatientrightstypePatientrights").
+			Unique(),
+
+		/*
+		edge.To("Patientrights___", ___.Type),
+
+		*/
+
+		edge.From("PatientrightsInsurance", Insurance.Type).
+			Ref("InsurancePatientrights").
+			Unique(),
+
+		edge.From("PatientrightsUser", User.Type).
+			Ref("UserPatientrights").
+			Unique(),
+
+	}
 }
