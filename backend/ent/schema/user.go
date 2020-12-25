a660586bb3beb1a23e0960d85c199f0743e4704c
paonratp <60309550+paonratp@users.noreply.github.com>
2020-12-26 02:08:54 +0700

    Update user.go

--- backend/ent/schema/user.go
+++ backend/ent/schema/user.go
@@ -24,6 +24,5 @@ func (User) Edges() []ent.Edge {
 	return []ent.Edge{
 		edge.To("financier", Financier.Type).StorageKey(edge.Column("user_id")).Unique(), 
 		edge.To("historytaking", Historytaking.Type).StorageKey(edge.Column("user_id")).Unique(), 
-		edge.To("patientrecord", Patientrecord.Type).StorageKey(edge.Column("user_id")).Unique(),
 	}
 }
