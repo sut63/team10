0c6d956a1937eb45ea100582ace80dba84b7e114
m16_z <m16_za@hotmail.com>
2020-12-26 23:44:27 +0700

    entity Treatment

---
+++ backend/ent/schema/treatment.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+	"github.com/facebookincubator/ent/schema/edge"
+)
+
+// Treatment holds the schema definition for the Treatment entity.
+type Treatment struct {
+	ent.Schema
+}
+
+// Fields of the Treatment.
+func (Treatment) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("treatment").NotEmpty(),
+		field.Time("datetime"),
+	}
+}
+
+// Edges of the Treatment.
+func (Treatment) Edges() []ent.Edge {
+	return []ent.Edge{
+	edge.From("typetreatment", Typetreatment.Type).Ref("treatment").Unique(),
+	edge.From("patientrecord", Patientrecord.Type).Ref("treatment").Unique(),
+	edge.From("doctorinfo", Doctorinfo.Type).Ref("treatment").Unique(),
+	}
+}
