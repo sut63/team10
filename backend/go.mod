9e8184ce7305f00d6ffd25bc207e4a7eb5411ab5
khatadet <ponpang39170@gmail.com>
2021-01-05 22:23:20 +0700

    ปรับปรุง main

--- backend/go.mod
+++ backend/go.mod
@@ -4,14 +4,18 @@ go 1.14
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
-	github.com/facebook/ent v0.5.3
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/facebookincubator/ent v0.2.7
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/google/uuid v1.1.2 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/mattn/go-sqlite3 v1.14.5
 	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
 	github.com/swaggo/gin-swagger v1.3.0
+	github.com/swaggo/swag v1.5.1
+	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/tools v0.0.0-20200904185747-39188db58858 // indirect
 
 )
--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,7 @@ import (
    "context"
    "fmt"
    "log"
-   "time"
+  
   
  
    "github.com/team10/app/controllers"
@@ -19,10 +19,7 @@ import (
    "github.com/team10/app/ent/abilitypatientrights"
 
 
-   "github.com/team10/app/ent/insurance"
-	"github.com/team10/app/ent/patientrecord"
-	"github.com/team10/app/ent/patientrightstype"
-	"github.com/team10/app/ent/medicalrecordstaff"
+  
 
 
 )
@@ -218,74 +215,7 @@ func main() {
 		SetPatientrightstypeAbilitypatientrights(a).
 		Save(context.Background())
 	}
-	//***********************************************************************************
-	// Set Patientrightstypes Data
-	patientrightss := Patientrightss{
-		Patientrights : []Patientrights{
-			Patientrights{1,1,1,1},
-			Patientrights{1,1,1,1},
-			Patientrights{1,1,1,1},
-			Patientrights{1,1,1,1},
-		},
-	}
-
-	for _, p := range patientrightss.Patientrights {
-
-		
-	Patientrightstype, err := client.Patientrightstype.
-		Query().
-		Where(patientrightstype.IDEQ(int(p.Patientrightstype))).
-		Only(context.Background())
-
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		Insurance, err := client.Insurance.
-		Query().
-		Where(insurance.IDEQ(int(p.Insurance))).
-		Only(context.Background())
-
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-	Patientrecord, err := client.Patientrecord.
-		Query().
-		Where(patientrecord.IDEQ(int(p.Patientrecord))).
-		Only(context.Background())
-
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-	Medicalrecordstaff, err := client.Medicalrecordstaff.
-		Query().
-		Where(medicalrecordstaff.IDEQ(int(p.Medicalrecordstaff))).
-		Only(context.Background())
-
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
 	
-	t := time.Now().Local()
-	client.Patientrights.
-		Create().
-		SetPermissionDate(t).
-		SetPatientrightsPatientrightstype(Patientrightstype).
-		SetPatientrightsPatientrecord(Patientrecord).
-		SetPatientrightsMedicalrecordstaff(Medicalrecordstaff).
-		SetPatientrightsInsurance(Insurance).
-		Save(context.Background())
-
-	
-	}
-    //***********************************************************************
    //-------------------------------------------------------------------
 
    router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
