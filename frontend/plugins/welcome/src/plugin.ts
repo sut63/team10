fc645c3fab1546c931c0917a6b9b5957fdd797c5
khatadet <ponpang39170@gmail.com>
2021-01-06 18:29:42 +0700

    ปก้ไขโครงสร้าง UI

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -3,9 +3,9 @@ import WelcomePage from './components/WelcomePage';
 import WatchVideo from './components/WatchVideo'
 import SignIn from './components/SignIn'
 
-
+//createHistorytaking
 import  create_Patientrights from './components/create_patientrights';
-
+import  createHistorytaking from './components/createHistorytaking';
 
 export const plugin = createPlugin({
   id: 'welcome',
@@ -14,6 +14,6 @@ export const plugin = createPlugin({
     router.registerRoute('/watch_video', WatchVideo);
     router.registerRoute('/signin', SignIn);
     router.registerRoute('/create_Patientrights', create_Patientrights);
-
+    router.registerRoute('/createHistorytaking', createHistorytaking);
   },
 });
