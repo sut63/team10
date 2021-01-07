66d8bac57bd9f4413ba5a3130a05c7bdbf28c0d5
paonratp <paonrat.p@gmail.com>
2021-01-07 17:38:48 +0700

    update main.go

--- backend/main.go
+++ backend/main.go
@@ -504,6 +504,22 @@ func main() {
 			SetSymptomseverity(ss.Symptomseverity).
 			Save(context.Background())
 	}
+	//Set Department data
+	departments := Departments{
+		Department: []Department{
+			Department{"ตาคอหู"},
+			Department{"กระดูก"},
+			Department{"อายุรกรรม"},
+		},
+	}
+
+	for _, d := range departments.Department {
+		client.Department.
+			Create().
+			SetDepartment(d.Department).
+			Save(context.Background())
+	}
+	
 	//^^^*******************************************************************^^^
 
 	// Set Postman By Bill System
