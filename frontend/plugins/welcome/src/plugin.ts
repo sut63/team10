15614cabc9fbed1ff3731269775cabf2b03e98c3
khatadet <ponpang39170@gmail.com>
2021-01-12 23:10:59 +0700

    แก้ไข UI

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -3,6 +3,7 @@ import WelcomePage from './components/WelcomePage';
 import SignIn from './components/SignIn'
 
 import create_Patientrights from './components/create_patientrights';
+import Table_patientrights from './components/Table_patientrights';
 import Historytaking from './components/Historytaking';
 import tableHistorytaking from './components/tableHistorytaking';
 import createHistorytaking from './components/createHistorytaking';
@@ -47,6 +48,7 @@ export const plugin = createPlugin({
       }
       if (Status == 'Med' || Status == 'Root' ) {
         router.registerRoute('/create_Patientrights', create_Patientrights);
+        router.registerRoute('/Table_patientrights', Table_patientrights);
         router.registerRoute('/createPatientrecord', createPatientrecord);
         router.registerRoute('/Patientrecord', Patientrecord);
         router.registerRoute('/tablePatientrecord', tablePatientrecord);
